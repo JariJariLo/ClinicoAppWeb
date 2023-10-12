bc23c99c8925be6d9123060a6798b172532cfbb2
KrisJari <146730448+JariJariLo@users.noreply.github.com>
2023-10-12 13:17:24 +0200

    comentarios en validacion

--- phparchivos/validar.php
+++ phparchivos/validar.php
@@ -5,7 +5,8 @@ $apellido1=$_POST['apellido1'];
 $apellido2=$_POST['apellido2'];
 $contraseña=$_POST['id'];*/
 
-  if(isset($_POST['nombre'])) {
+//estas son tus variables para rellenar los campos de la consulta
+ if(isset($_POST['nombre'])) {
     $paciente = $_POST['nombre'];
   }
    else {
@@ -36,17 +37,16 @@ $contraseña=$_POST['id'];*/
 
 session_start();
 $_SESSION['paciente']=$paciente;
-
+//aqui teconectas con tus datos a la base de datos
 $conexion=mysqli_connect("localhost:3308","root","","doctores");
-
+//consulta sql la puedes modificar con los datos que quieras 
 $consulta="SELECT * from personasficha where nombre='$paciente' and id='$contraseña'";
 $resultado=mysqli_query($conexion,$consulta);
 
 $filas=mysqli_num_rows($resultado);
-
+//aqui si todo va bien te lleva a una pagina despues de loguearte en este caso es home.php la puedes modificar si quieres
 if($filas){
 header("location:home.php");
-
 }
         else{
 
